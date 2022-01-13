df2a9b7d1892aa9975bfedc58a640ccb1dd46732
Kyle <me@proximity.sh>
2022-01-13 13:24:10 -0800

    Update entrypoint.sh

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -4,11 +4,14 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-# Print Node.js Version
+cd /home/container || exit 1
+
+# Print Node.js version
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mnode -v\n"
 node -v
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 printf "\033[1m\033[33mserver@sullivanshosting~ \033[0m%s\n" "$MODIFIED_STARTUP"
 
 # Run the Server
