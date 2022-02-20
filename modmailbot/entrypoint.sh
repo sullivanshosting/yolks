273a740ce1ba54da0bf22497f365d10e95633fe1
Kyle <me@proximity.sh>
2022-02-19 19:55:27 -0800

    Update entrypoint.sh

--- modmailbot/entrypoint.sh
+++ modmailbot/entrypoint.sh
@@ -1,13 +1,24 @@
-#!/bin/ash
+TZ=${TZ:-UTC}
+export TZ
 
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+export INTERNAL_IP
 
+# Switch to the container's working directory
 cd /home/container || exit 1
 
-printf "\033[1m\033[33mserver@SullivansHosting~ \033[0mnode -v\n"
+# Print Node.js version
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mnode -v\n"
 node -v
 
-MODIFIED_STARTUP=$(echo "node /home/container/src/index.js" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-printf "\033[1m\033[33mserver@sullivanshosting~ \033[0m%s\n" "Starting ModMailBot"
-# Run the Server
-exec env ${MODIFIED_STARTUP}
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+# shellcheck disable=SC2086
+exec env ${PARSED}
