3868945f86566f05148285ef1b0090aab7881958
Kyle <me@proximity.sh>
2022-02-19 19:00:16 -0800

    Update entrypoint.sh

--- modmailbot/entrypoint.sh
+++ modmailbot/entrypoint.sh
@@ -11,13 +11,14 @@ printf "\033[1m\033[33mserver@SullivansHosting~ \033[0mnode -v\n"
 node -v
 
 # Replace Startup Variables
-PLUGINS=(${MM_PLUGINS})
-BAR=""
+#PLUGINS=(${MM_PLUGINS})
+#BAR=""
 
-for index in ${!PLUGINS[*]}
-do
-    BAR="$BAR||./mmplugins/${PLUGINS[$index]}/index.js"
-done
+#for index in ${!PLUGINS[*]}
+#do
+#    BAR="$BAR||./mmplugins/${PLUGINS[$index]}/index.js"
+#done
+printf "${MM_PLUGINS}"
 MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 printf "\033[1m\033[33mserver@sullivanshosting~ \033[0m%s\n" "Starting ModMailBot"
 #config="MM_TOKEN=${MM_TOKEN} MM_MAIN_SERVER_ID=${MM_MAIN_SERVER_ID} MM_INBOX_SERVER_ID=${MM_INBOX_SERVER_ID} MM_LOG_CHANNEL_ID=${MM_LOG_CHANNEL_ID}"
