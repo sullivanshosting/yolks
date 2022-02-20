04529993cfb8fc43f6eae08ebc164d0eef153e06
Kyle <me@proximity.sh>
2022-02-19 21:31:48 -0800

    FUCKKKSL

--- modmailbot/entrypoint.sh
+++ modmailbot/entrypoint.sh
@@ -11,6 +11,6 @@ node -v
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
-
+config="MM_TOKEN=${TOKEN} MM_MAIN_SERVER_ID=${MAIN_SERVER_ID} MM_INBOX_SERVER_ID=${INBOX_SERVER_ID} MM_LOG_CHANNEL_ID=${LOG_CHANNEL_ID}"
 # Run the Server
-eval ${MODIFIED_STARTUP}
+eval ${CONFIG} ${MODIFIED_STARTUP}
