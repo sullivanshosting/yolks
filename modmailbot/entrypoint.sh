b70d7443a6f751bdae35fc7498d38b82fdbb3332
Kyle <me@proximity.sh>
2022-02-19 21:22:32 -0800

    FUCKKKSL

--- modmailbot/entrypoint.sh
+++ modmailbot/entrypoint.sh
@@ -7,6 +7,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 # Print Node.js Version
 node -v
 
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
