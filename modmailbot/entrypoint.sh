76bcb5ffbd30dbc762c89ca47c9044b89b03071e
Kyle <me@proximity.sh>
2022-02-19 18:40:27 -0800

    Update entrypoint.sh

--- modmailbot/entrypoint.sh
+++ modmailbot/entrypoint.sh
@@ -18,8 +18,8 @@ for index in ${!PLUGINS[*]}
 do
     BAR="$BAR||./mmplugins/${PLUGINS[$index]}/index.js"
 done
-MODIFIED_STARTUP=$(echo "MM_TOKEN=${MM_TOKEN} MM_MAIN_SERVER_ID=${MM_MAIN_SERVER_ID} MM_INBOX_SERVER_ID=${MM_INBOX_SERVER_ID} MM_LOG_CHANNEL_ID=${MM_LOG_CHANNEL_ID} ${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 printf "\033[1m\033[33mserver@sullivanshosting~ \033[0m%s\n" "Starting ModMailBot"
-
+config="MM_TOKEN=${MM_TOKEN} MM_MAIN_SERVER_ID=${MM_MAIN_SERVER_ID} MM_INBOX_SERVER_ID=${MM_INBOX_SERVER_ID} MM_LOG_CHANNEL_ID=${MM_LOG_CHANNEL_ID}"
 # Run the Server
-exec env ${MODIFIED_STARTUP}
+exec env ${config} ${MODIFIED_STARTUP}
